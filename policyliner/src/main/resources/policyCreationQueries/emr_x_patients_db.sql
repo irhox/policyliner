e19881711602e509472d751b415435f1db15541f
Irild Hoxhallari <irhox100@gmail.com>
2025-11-24 23:28:30 +0100

    - changed the policyCreationQueries

--- policyliner/src/main/resources/policyCreationQueries/emr_x_patients_db.sql
+++ policyliner/src/main/resources/policyCreationQueries/emr_x_patients_db.sql
@@ -1,103 +1,70 @@
--- 1. Medical Staff Policy
-CREATE OR REPLACE VIEW patients_admissions_diagnosis_labs_policy AS
-WITH labs_grouped AS (
+
+-- Disclosure Policies used in the Experiments Chapter of the master thesis
+-- 1. Public Health Researcher Policy
+CREATE VIEW patients_admissions_diagnosis_policy AS
     SELECT
-        admissionid,
-        patientid,
-        STRING_AGG(DISTINCT labname, ', ') AS labnames,
-        STRING_AGG(DISTINCT labunits, ', ') AS labunits,
-        STRING_AGG(DISTINCT (labvalue::text), ', ') AS labvalues
-    FROM labs
-    GROUP BY admissionid, patientid
-)
-SELECT  randomized_response_patientgender(patientgender) AS patientgender,
-        generalize_date(to_date(to_char(patientdateofbirth, 'DD/MM/YYYY'), 'DD/MM/YYYY'), 'YEAR') AS to_date_to_char_patientdateofbirth,
+        bucketize_age(patientdateofbirth, '5 years') AS patientdateofbirth,
+        randomized_response_patientrace(patientrace) AS patientrace,
+        randomized_response_patientmaritalstatus(patientmartialstatus) AS patientmartialstatus,
+        randomized_response_patientgender(patientgender) AS patientgender,
+        bucketize(patientpopulationpercentagebelowpoverty, 5) AS patientpopulationpercentagebelowpoverty,
+        generalize_primarydiagnosiscode(primarydiagnosiscode) AS primarydiagnosiscode,
         generalize_date(to_date(to_char(admissionstartdate, 'DD/MM/YYYY'), 'DD/MM/YYYY'), 'MONTH') AS to_date_to_char_admissionstartdate,
-        generalize_date(to_date(to_char(admissionenddate, 'DD/MM/YYYY'), 'DD/MM/YYYY'), 'MONTH') AS to_date_to_char_admissionenddate,
-        patients.patientid AS patients_patientid,
-        admissions.admissionid AS admissions_admissionid,
-        primarydiagnosiscode AS primarydiagnosiscode,
-        primarydiagnosisdescription AS primarydiagnosisdescription,
-        labnames AS labnames,
-        labvalues AS labvalues,
-        labunits AS labunits
-FROM patients
-         JOIN admissions ON admissions.patientid = patients.patientid
-         JOIN diagnosis ON diagnosis.admissionid = admissions.admissionid
-         JOIN labs_grouped ON labs_grouped.admissionid = diagnosis.admissionid;
+        generalize_date(to_date(to_char(admissionenddate, 'DD/MM/YYYY'), 'DD/MM/YYYY'), 'MONTH') AS to_date_to_char_admissionenddate
+    FROM patients
+        JOIN admissions ON admissions.patientid = patients.patientid
+        JOIN diagnosis ON diagnosis.patientid = admissions.patientid;
 
--- 2. Front Desk Staff Policy
-CREATE OR REPLACE VIEW patients_admissions_policy
-    AS SELECT  generalize_date(to_date(to_char(admissionstartdate, 'DD/MM/YYYY'), 'DD/MM/YYYY'), 'MONTH') AS to_date_to_char_admissionstartdate,
-               generalize_date(to_date(to_char(admissionenddate, 'DD/MM/YYYY'), 'DD/MM/YYYY'), 'MONTH') AS to_date_to_char_admissionenddate,
-               randomized_response_patientgender(patientgender) AS patientgender,
-               randomized_response_patientmaritalstatus(patientmaritalstatus) AS patientmaritalstatus,
-               randomized_response_patientlanguage(patientlanguage) AS patientlanguage,
-               patients.patientid AS patients_patientid,
-               admissions.admissionid AS admissions_admissionid
-FROM patients
-    JOIN admissions ON admissions.patientid = patients.patientid;
 
--- 3. Lab Technician
-CREATE VIEW labs_policy
-    AS SELECT  suppress(patientid) AS patientid,
-               generalize_labname(labname) AS labname,
-               generalize_date(to_date(to_char(labdatetime, 'DD/MM/YYYY'), 'DD/MM/YYYY'), 'MONTH') AS to_date_to_char_labdatetime,
-               admissionid AS admissionid,
-               labvalue AS labvalue,
-               labunits AS labunits
-       FROM labs;
 
--- 4. Public Health Researcher
-CREATE VIEW patients_admissions_diagnosis_policy
-    AS SELECT  suppress(patients.patientid) AS patients_patientid,
-               suppress(admissions.admissionid) AS admissions_admissionid,
-               randomized_response_patientlanguage(patientlanguage) AS patientlanguage,
-               generalize_primarydiagnosiscode(primarydiagnosiscode) AS primarydiagnosiscode,
-               patientpopulationpercentagebelowpoverty AS patientpopulationpercentagebelowpoverty
-       FROM patients
-           JOIN admissions ON admissions.patientid = patients.patientid
-           JOIN diagnosis ON diagnosis.admissionid = admissions.admissionid;
+-- Examples of other Disclosure Policies
+-- 1. Front Desk Staff Policy
+CREATE VIEW patients_admissions_policy AS
+    SELECT  generalize_date(to_date(to_char(admissionstartdate, 'DD/MM/YYYY'), 'DD/MM/YYYY'), 'MONTH') AS to_date_to_char_admissionstartdate,
+            generalize_date(to_date(to_char(admissionenddate, 'DD/MM/YYYY'), 'DD/MM/YYYY'), 'MONTH') AS to_date_to_char_admissionenddate,
+            randomized_response_patientgender(patientgender) AS patientgender,
+            randomized_response_patientmaritalstatus(patientmartialstatus) AS patientmartialstatus,
+            randomized_response_patientlanguage(patientlanguage) AS patientlanguage,
+            patients.patientid AS patients_patientid,
+            admissions.admissionid AS admissions_admissionid
+    FROM patients
+        JOIN admissions ON admissions.patientid = patients.patientid;
 
--- 5. Audit Officer
-CREATE VIEW patients_admissions_diagnosis_labs_policy790
-    AS SELECT  generalize_primarydiagnosiscode(patients.patientid) AS patients_patientid,
-               bucketize(admissions.admissionid, 10) AS admissions_admissionid,
-               generalize_primarydiagnosiscode(primarydiagnosiscode) AS primarydiagnosiscode,
-               generalize_labname(labname) AS labname
-       FROM patients
-           JOIN admissions ON admissions.patientid = patients.patientid
-           JOIN diagnosis ON diagnosis.admissionid = admissions.admissionid
-           JOIN labs ON labs.admissionid = diagnosis.admissionid
-   LIMIT 1000000;
+-- 2. Lab Technician
+CREATE VIEW labs_policy AS
+    SELECT  generalize_labname(labname) AS labname,
+            generalize_date(to_date(to_char(labdatetime, 'DD/MM/YYYY'), 'DD/MM/YYYY'), 'MONTH') AS to_date_to_char_labdatetime,
+            labvalue AS labvalue,
+            labunits AS labunits
+    FROM labs;
 
--- 6. Specialist Doctor
-CREATE VIEW patients_admissions_diagnosis_policy901
-    AS SELECT  generalize_primarydiagnosiscode(patients.patientid) AS patients_patientid,
-               bucketize(admissions.admissionid, 10) AS admissions_admissionid,
-               generalize_primarydiagnosiscode(primarydiagnosiscode) AS primarydiagnosiscode,
-               generalize_date(to_date(to_char(admissionstartdate, 'DD/MM/YYYY'), 'DD/MM/YYYY'), 'MONTH') AS to_date_to_char_admissionstartdate,
-               generalize_date(to_date(to_char(admissionenddate, 'DD/MM/YYYY'), 'DD/MM/YYYY'), 'MONTH') AS to_date_to_char_admissionenddate,
-               primarydiagnosisdescription AS primarydiagnosisdescription
-       FROM patients
-           JOIN admissions ON admissions.patientid = patients.patientid
-           JOIN diagnosis ON diagnosis.admissionid = admissions.admissionid;
+-- 3. Specialist Doctor
+CREATE VIEW patients_admissions_diagnosis_policy944 AS
+    SELECT  generalize_primarydiagnosiscode(patients.patientid) AS patients_patientid,
+            bucketize(admissions.admissionid, 5) AS admissions_admissionid,
+            generalize_primarydiagnosiscode(primarydiagnosiscode) AS primarydiagnosiscode,
+            generalize_date(to_date(to_char(admissionstartdate, 'DD/MM/YYYY'), 'DD/MM/YYYY'), 'MONTH') AS to_date_to_char_admissionstartdate,
+            generalize_date(to_date(to_char(admissionenddate, 'DD/MM/YYYY'), 'DD/MM/YYYY'), 'MONTH') AS to_date_to_char_admissionenddate,
+            primarydiagnosisdescription AS primarydiagnosisdescription
+    FROM patients
+        JOIN admissions ON admissions.patientid = patients.patientid
+        JOIN diagnosis ON diagnosis.patientid = admissions.patientid;
 
 -- 7. Patient Demographics
-CREATE VIEW patients_policy
-    AS SELECT  randomized_response_patientgender(patientgender) AS patientgender,
-               randomized_response_patientmaritalstatus(patientmaritalstatus) AS patientmaritalstatus,
-               randomized_response_patientlanguage(patientlanguage) AS patientlanguage,
-               randomized_response_patientrace(patientrace) AS patientrace,
-               patientid AS patientid,
-               patientdateofbirth AS patientdateofbirth
-       FROM patients;
+CREATE VIEW patients_policy AS
+    SELECT  randomized_response_patientgender(patientgender) AS patientgender,
+            randomized_response_patientmaritalstatus(patientmartialstatus) AS patientmartialstatus,
+            randomized_response_patientlanguage(patientlanguage) AS patientlanguage,
+            randomized_response_patientrace(patientrace) AS patientrace,
+            patientid AS patientid,
+            patientdateofbirth AS patientdateofbirth
+    FROM patients;
 
 -- 8. Diagnosis per Admission
-CREATE VIEW admissions_diagnosis_policy
-    AS SELECT  generalize_primarydiagnosiscode(admissions.patientid) AS admissions_patientid,
-               generalize_primarydiagnosiscode(primarydiagnosiscode) AS primarydiagnosiscode,
-               admissions.admissionid AS admissions_admissionid,
-               primarydiagnosisdescription AS primarydiagnosisdescription
-       FROM admissions
-           JOIN diagnosis ON diagnosis.admissionid = admissions.admissionid
+CREATE VIEW admissions_diagnosis_policy AS
+    SELECT  generalize_primarydiagnosiscode(admissions.patientid) AS admissions_patientid,
+            generalize_primarydiagnosiscode(primarydiagnosiscode) AS primarydiagnosiscode,
+            admissions.admissionid AS admissions_admissionid,
+            primarydiagnosisdescription AS primarydiagnosisdescription
+    FROM admissions
+        JOIN diagnosis ON diagnosis.patientid = admissions.patientid;
